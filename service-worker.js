5ad0acaa42a0b23653ea81d00ed4a9ea33f294f1
kangJ-collab <efde234@gmail.com>
2026-07-17 17:04:39 +0900

    service-worker.js 업데이트

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 javascript
-const CACHE_VERSION = 'shiftcalendar-pwa-v7';
+const CACHE_VERSION = 'shiftcalendar-pwa-v8';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
